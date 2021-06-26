2886967012cdab240f399b4296a7a64f4231b63b
Mizanur Rahman <mizzanur007@gmail.com>
2021-06-26 22:55:40 +0600

    finished

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -2,14 +2,12 @@ import { Link } from 'react-router-dom';
 import './post.css';
 
 export default function Post({ post }) {
+  const PF = 'http://localhost:5000/images/';
+
   return (
     <div className="post">
       {post.photo && (
-        <img
-          className="postImg"
-          src="https://images.unsplash.com/photo-1511497584788-876760111969?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80"
-          alt="post pic"
-        />
+        <img className="postImg" src={PF + post.photo} alt="post pic" />
       )}
 
       <div className="postInfo">
--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -1,12 +1,15 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { Context } from '../../context/Context';
 import './singlepost.css';
 
 export default function SinglePost() {
+  const { user } = useContext(Context);
   const location = useLocation();
   const path = location.pathname.split('/')[2];
   const [post, setPost] = useState({});
+  const PF = 'http://localhost:5000/images/';
 
   useEffect(() => {
     const getPost = async () => {
@@ -15,19 +18,33 @@ export default function SinglePost() {
     };
     getPost();
   }, [path]);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${path}`, {
+        data: { username: user.username },
+      });
+      window.location.replace('/');
+    } catch (err) {}
+  };
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
         {post.photo && (
-          <img src={post.photo} alt="" className="singlePostImg" />
+          <img src={PF + post.photo} alt="" className="singlePostImg" />
         )}
 
         <h1 className="singlePostTitle">
           {post.title}
-          <div className="singlePostEdit">
-            <i className="singlePostIcon far fa-edit"></i>
-            <i className="singlePostIcon far fa-trash-alt"></i>
-          </div>
+          {post.username === user?.username && (
+            <div className="singlePostEdit">
+              <i className="singlePostIcon far fa-edit"></i>
+              <i
+                className="singlePostIcon far fa-trash-alt"
+                onClick={handleDelete}
+              ></i>
+            </div>
+          )}
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
